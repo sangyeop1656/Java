5882137794f01db81072aeb10663fad7b12bc08e
김상엽 <kq1kq@naver.com>
2024-10-28 13:23:17 +0900

    텍스트 필드, 버튼 크기 조절

--- test01/test01/test01.java
+++ test01/test01/test01.java
@@ -9,6 +9,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 
 public class test01 extends JFrame {
@@ -16,7 +17,7 @@ public class test01 extends JFrame {
 	 	JButton[] buttons = new JButton[24]; 
 
 	    public test01() {
-	        setSize(500, 400); 
+	        setSize(350, 550); 
 	        setTitle("계산기"); 
 	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	  
@@ -26,6 +27,8 @@ public class test01 extends JFrame {
 	        text.setFont(new Font("0",Font.PLAIN, 24));
 	        text.setEditable(false); 
 	        text.setBackground(Color.white);         
+	        text.setHorizontalAlignment(SwingConstants.RIGHT);
+	        text.setPreferredSize(new java.awt.Dimension(500, 100));
 	        add(panel,BorderLayout.CENTER);
 	        add(text, BorderLayout.NORTH); 
 	     
@@ -33,8 +36,8 @@ public class test01 extends JFrame {
 	       
 	        Color buttonColor = Color.WHITE;
 	        String[] buttonLabels = {
-	            "%", "CE", "C", "", 
-	            "1/x", "x^", "2루트x", "%",
+	            "%", "CE", "C", "지우기", 
+	            "1/x", "x^", "2루트x", "÷",
 	             "7", "8", "9", "x", 
 	            "4", "5", "6", "-", 
 	            "1", "2", "3", "+",
@@ -43,7 +46,8 @@ public class test01 extends JFrame {
 	        
 	        for (int i = 0; i < buttons.length; i++) {
 	            buttons[i] = new JButton(buttonLabels[i]);
-	            buttons[i].setBackground(buttonColor);        
+	            buttons[i].setBackground(buttonColor);       
+	            buttons[i].setPreferredSize(new java.awt.Dimension(100, 30));
 	            
 	            panel.add(buttons[i]); 
 	        }
